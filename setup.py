1e0bbc7e3fc6dcb0777d38a72d64f00d90517f41
shosatojp <me@shosato.jp>
2022-10-05 03:25:55 +0000

    ライセンス修正（pip installに失敗する問題を修正）

--- setup.py
+++ setup.py
@@ -8,29 +8,20 @@ def _requirements():
     return [name.rstrip() for name in open(os.path.join(root_dir, 'requirements.txt')).readlines()]
 
 
-print(setuptools.find_packages())
 setuptools.setup(
     name='limedio',
     packages=setuptools.find_packages(),
-
-    version='0.0.1',
-
-    license=license,
-
-
+    version='0.0.3',
     install_requires=_requirements(),
-
     author='shosatojp',
     author_email='me@shosato.jp',
-
     url='https://github.com/uec-world-dominators/limedio',
-
     description='UEC Auth',
     long_description='',
     keywords='',
-
     classifiers=[
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
     ],
     python_requires='>=3.6',
 )
